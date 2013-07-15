1a4f71e851c83d72ced06a177ef2a16665b0e48f
guoqiao <guoqiao@gmail.com>
2013-07-15 15:11:30 +0800

    add locker path settings

--- mailer/engine.py
+++ mailer/engine.py
@@ -33,7 +33,7 @@ def prioritize():
     """
     Yield the messages in the queue in the order they should be sent.
     """
-    
+
     while True:
         while Message.objects.high_priority().count() or Message.objects.medium_priority().count():
             while Message.objects.high_priority().count():
@@ -51,9 +51,12 @@ def send_all():
     """
     Send all eligible messages in the queue.
     """
-    
-    lock = FileLock("send_mail")
-    
+    root = getattr(settings,'PROJ_ROOT')
+    import os
+    locker = os.path.join(root,"send_mail_lock")
+
+    lock = FileLock(locker)
+
     logging.debug("acquiring lock...")
     try:
         lock.acquire(LOCK_WAIT_TIMEOUT)
@@ -64,13 +67,13 @@ def send_all():
         logging.debug("waiting for the lock timed out. quitting.")
         return
     logging.debug("acquired.")
-    
+
     start_time = time.time()
-    
+
     dont_send = 0
     deferred = 0
     sent = 0
-    
+
     try:
         connection = None
         for message in prioritize():
@@ -95,7 +98,7 @@ def send_all():
         logging.debug("releasing lock...")
         lock.release()
         logging.debug("released.")
-    
+
     logging.info("")
     logging.info("%s sent; %s deferred;" % (sent, deferred))
     logging.info("done in %.2f seconds" % (time.time() - start_time))
@@ -105,7 +108,7 @@ def send_loop():
     Loop indefinitely, checking queue at intervals of EMPTY_QUEUE_SLEEP and
     sending messages if any are on queue.
     """
-    
+
     while True:
         while not Message.objects.all():
             logging.debug("sleeping for %s seconds before checking queue again" % EMPTY_QUEUE_SLEEP)
